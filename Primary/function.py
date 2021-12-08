aca8b544bd3342bdb228701f790c434e8913ed1f
JanzenXu <thejanzenxu@outlook.com>
2021-12-08 12:50:27 +0800

    1.3

--- Primary/function.py
+++ Primary/function.py
@@ -1,11 +1,10 @@
 import cv2 as cv
 import numpy as np
-import os
 
 
 def colorRecog(imgOri, color):  # 根据对方颜色进行二值化
     colorDict = {
-        'red': [np.array([0, 55, 100]), np.array([80, 255, 255])],
+        'red': [np.array([0, 100, 100]), np.array([55, 255, 255])],
         'blue': [np.array([0, 60, 60]), np.array([255, 255, 255])]
     }
 
@@ -24,7 +23,7 @@ def colorRecog(imgOri, color):  # 根据对方颜色进行二值化
     return imgOut
 
 
-def objectiveDetect(imgBinary, imgOri):  # 识别轮廓搜索装甲板并筛选
+def objectiveDetect(imgBinary):  # 识别轮廓搜索装甲板并筛选
     h, w = imgBinary.shape
 
     # 初始化包含轮廓对的列表
@@ -36,23 +35,22 @@ def objectiveDetect(imgBinary, imgOri):  # 识别轮廓搜索装甲板并筛选
 
     # 查找轮廓
     cnts, hier = cv.findContours(
-        imgBinary, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-    
-    retval = cnts is not None
+        imgBinary, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+
+    retval = cnts != ()
     if retval:
-        retval = False
+        retval = False  # 中途退出的返回值全为False
         for cnt in cnts:
-            dic = dict()
             if cv.contourArea(cnt) > 3396 or cv.contourArea(cnt) < 6:
                 continue
 
+            dic = dict()
             area = cv.contourArea(cnt)
             ellipse = cv.fitEllipse(cnt)
             ellCenter, ellShape, ellAngle = ellipse
 
             rect = cv.minAreaRect(cnt)
             coordinates = np.int0(cv.boxPoints(rect))
-            # cv.polylines(img,[ coordinates], True, (255, 0, 0), 1)
 
             dic['area'] = area
             dic['ellCenter'] = ellCenter
@@ -61,6 +59,7 @@ def objectiveDetect(imgBinary, imgOri):  # 识别轮廓搜索装甲板并筛选
             dic['coordinates'] = coordinates
             dataRaw.append(dic)
 
+        # 筛选符合条件的轮廓
         for i in range(len(dataRaw)):
             height = dataRaw[i]['ellShape'][0]
             width = dataRaw[i]['ellShape'][1]
@@ -68,18 +67,15 @@ def objectiveDetect(imgBinary, imgOri):  # 识别轮廓搜索装甲板并筛选
             angle = dataRaw[i]['ellAngle']
 
             if 4 >= height/width >= 0.2 and area > 18:
-                if angle < 45 or angle > 135:
+                if angle < 30 or angle > 150:
                     dataCheck.append(dataRaw[i])
 
+        # 对筛选出的轮廓按中心点高度排序
         dataCheck = sorted(
             dataCheck, key=lambda dataCheck: dataCheck['ellCenter'][1])
 
+        # 查找符合条件的轮廓对
         for i in range(len(dataCheck)):
-            coordinates = dataCheck[i]['coordinates']
-            # cv.polylines(img, [coordinates], True, (0, 255, 255))
-            # cv.putText(img, str(dataCheck[i]['ellAngle']),
-            #            coordinates[0], 0, 0.5, (0, 0, 255))
-
             j = i+1
             while j < len(dataCheck):
                 ellX1, ellY1 = dataCheck[i]['ellCenter']
@@ -100,42 +96,42 @@ def objectiveDetect(imgBinary, imgOri):  # 识别轮廓搜索装甲板并筛选
                                         (dataCheck[i], dataCheck[j]))
                 j += 1
 
+        # 对共享一个轮廓的两组轮廓对进行筛选
         dataMatchPro = dataMatch.copy()
         for i in range(len(dataMatch)):
             center2 = dataMatch[i][1]['ellCenter']
-            angle1 = dataMatch[i][0]['ellAngle']+0.000000001
-            angle2 = dataMatch[i][1]['ellAngle']+0.000000001
+            angle1 = dataMatch[i][0]['ellAngle']+0.0000000001
+            angle2 = dataMatch[i][1]['ellAngle']+0.0000000001
 
             j = i+1
             if j < len(dataMatch):
                 centerTest1 = dataMatch[j][0]['ellCenter']
-                angleTest1 = dataMatch[j][0]['ellAngle']+0.000000001
-                angleTest2 = dataMatch[j][1]['ellAngle']+0.000000001
+                angleTest1 = dataMatch[j][0]['ellAngle']+0.0000000001
+                angleTest2 = dataMatch[j][1]['ellAngle']+0.0000000001
                 if centerTest1 == center2:
                     if abs(angleTest1/angleTest2-1) > abs(angle1/angle2-1):
-                        dataMatchPro.pop(j)
+                        dataMatchPro[j] = 0
                     else:
-                        dataMatchPro.pop(i)
+                        dataMatchPro[i] = 0
+            while 0 in dataMatchPro:
+                dataMatchPro.remove(0)
 
-        if dataMatchPro is not None:
+        if dataMatchPro != []:
             retval = True
             for i in range(len(dataMatchPro)):
                 coordinates1 = dataMatchPro[i][0]['coordinates']
                 coordinates2 = dataMatchPro[i][1]['coordinates']
 
-                # cv.polylines(img, [coordinates1], True, (255, 255, 0))
-                # cv.polylines(img, [coordinates2], True, (255, 0, 255))
-
+                # 绘制包含一个轮廓对的最小矩形
                 matTemp = np.zeros((h, w), np.uint8)
                 cv.polylines(matTemp, [coordinates1], True, 255)
                 cv.polylines(matTemp, [coordinates2], True, 255)
                 cv.line(matTemp, coordinates1[0], coordinates2[0], 255)
                 cntTemp, hierTemp = cv.findContours(
                     matTemp, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+                # 找到外接矩形
                 rectX, rectY, rectW, rectH = cv.boundingRect(cntTemp[0])
 
-                cv.rectangle(imgOri, (rectX, rectY),
-                             (rectX+rectW, rectY+rectH), (255, 155, 0), 3)
                 rectOut.append((rectX, rectY, rectW, rectH))
 
     return retval, rectOut
--- Primary/main.py
+++ Primary/main.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 import numpy as np
 
-from function import colorRecog
+from function import colorRecog, objectiveDetect
 
 video = cv.VideoCapture('RMvideo/1.mp4')
 
@@ -14,5 +14,22 @@ cv.resizeWindow('res', (w-200, h-200))
 while video.isOpened():
     cap, frame = video.read()
     if cap:
-        frameRed = colorRecog(frame, 'red')
+        frameBinary = colorRecog(frame, 'red')
+        retval, rects = objectiveDetect(frameBinary)
 
+        if retval:
+            for rect in rects:
+                rectX, rectY, rectW, rectH = rect
+                cv.rectangle(frame, (rectX, rectY),
+                             (rectX+rectW, rectY+rectH), (255, 155, 0), 3)
+
+        cv.imshow('res', frame)
+        print(retval)
+
+        if cv.waitKey(1) & 0xff == ord('q'):
+            break
+    else:
+        break
+
+cv.destroyAllWindows()
+video.release()
